ef26afdd528562454abfafaaa49ae853fd842c06
Zineb Esso <74991230+DesignToWebsite@users.noreply.github.com>
2024-10-31 11:03:51 +0100

    api key

--- front-end/src/config/config.jsx
+++ front-end/src/config/config.jsx
@@ -1,6 +1,6 @@
 const config = {
-    youtubeApiKey : 'AIzaSyDQ0VyyNdyJMgEgLrsz9Qa_R3qi1q8twJM',
-    stripePublishKey : 'pk_test_51OUUUxHak1rgq3rARSzhIyJ1Xkmv6fzDJHUstxnMHfEyB64pCEj4nGqEiIQj04uvtQqTA9RvXwQIkid8S7eIhUck002XKMTqDL'
+    youtubeApiKey : 'YOUR_API_Key',
+    stripePublishKey : 'Strinpe_Key'
 
 };
 
